41979efca58d246139f21e644d694490d5dd0b21
rchaconalcantara <hola.hack09@gmail.com>
2024-05-28 05:49:55 -0400

    Testing with Jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,27 @@
 module.exports = {
   preset: 'jest-preset-angular',
-  globalSetup: 'jest-preset-angular/global-setup',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/src/test.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.html$',
+      astTransformers: {
+        before: ['jest-preset-angular/build/InlineFilesTransformer'],
+        after: ['jest-preset-angular/build/StripStylesTransformer'],
+      },
+    },
+  },
+  moduleNameMapper: {
+    'src/(.*)': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.(ts|html)$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  collectCoverage: true,
+  coverageReporters: ['html', 'text-summary'],
+  coverageDirectory: 'coverage/jest',
+  testEnvironment: 'jsdom',
 };
